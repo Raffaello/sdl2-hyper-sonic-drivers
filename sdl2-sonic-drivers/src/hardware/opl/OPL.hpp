efee33620f47f44a288d7919e7f413bdaee34a42
Raffaello Bertini <raffaellobertini@gmail.com>
2021-08-01 10:50:31 +0100

    [WIP] hardware pc-speaker, adlib

--- sdl2-sonic-drivers/src/hardware/opl/OPL.hpp
+++ sdl2-sonic-drivers/src/hardware/opl/OPL.hpp
@@ -9,15 +9,14 @@ namespace hardware
     /// </summary>
     namespace opl
     {
-        // shouldn't done from inheritance class instead?
         enum class ChipType
         {
             OPL2,
             OPL2_DUAL,
             OPL3,
-            //OPL2_SURROUND_OPL, /// OPL2 emulation from surroundopl code, using 2 wemu istances (OPL2_DUAL)
-            //OPL3_WEMU,         /// OPL3 emulation from wemuopl code (using OPL_3)
-            //OPL_woodyopl,      /// the only one containing hardware emulation to retro eng.
+            //OPL2_SURROUND_OPL, /// OPL2 emulation from surroundopl code, using 2 wemu mono istances (OPL2_DUAL)
+            //OPL3_WEMU,         /// OPL3 emulation from wemuopl code, woodyopl (using OPL_3)
+            //OPL_woodyopl,      /// the only one containing hardware emulation to retro eng (OPL_3).
             OPL_dbopl         /// dbopl file  like woodyopl but in integer instead of float.
         };
 
