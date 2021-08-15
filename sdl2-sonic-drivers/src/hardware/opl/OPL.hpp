0ce93801e6a81b25bb63583599d5911b4b5ca496
Raffaello Bertini <raffaellobertini@gmail.com>
2021-08-15 15:13:11 +0100

    [WIP] init opl re-work

--- sdl2-sonic-drivers/src/hardware/opl/OPL.hpp
+++ sdl2-sonic-drivers/src/hardware/opl/OPL.hpp
@@ -41,7 +41,7 @@ namespace hardware
             virtual void update(int16_t* buf, const int32_t samples) = 0;
             virtual void write(const int reg, const int val) = 0; // combined register select + data write
             //virtual void setchip(int n) // select OPL chip
-            //virtual void init() = 0; // reinitialize OPL chip(s)
+            virtual void init() = 0; // reinitialize OPL chip(s)
             virtual int32_t getSampleRate() const noexcept = 0;
         protected:
             ChipType _chip;
