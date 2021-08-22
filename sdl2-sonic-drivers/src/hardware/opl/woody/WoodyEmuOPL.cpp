e8a461d79cd71a13811ae020951cef107e9c39a5
Raffaello Bertini <raffaellobertini@gmail.com>
2021-08-22 23:27:45 +0100

    hardware/opl/woody restore

---
+++ sdl2-sonic-drivers/src/hardware/opl/woody/WoodyEmuOPL.cpp
@@ -0,0 +1,45 @@
+#include "WoodyEmuOPL.hpp"
+
+namespace hardware
+{
+    namespace opl
+    {
+        namespace woody
+        {
+            WoodyEmuOPL::WoodyEmuOPL(const int rate, const bool usestereo) noexcept
+                : OPL(ChipType::OPL3), _stereo(usestereo), _opl(rate)
+            {
+            }
+
+            void WoodyEmuOPL::update(int16_t* buf, const int32_t samples)
+            {
+                //      if(use16bit) samples *= 2;
+                if (_stereo) {
+                    _opl.adlib_getsample(buf, samples * 2);
+                } else {
+                    _opl.adlib_getsample(buf, samples);
+                }
+            }
+
+            // template methods
+            void WoodyEmuOPL::write(const int reg, const int val)
+            {
+                //if (_currentChip != 0) {
+                //    return;
+                //}
+
+                _opl.index = reg;
+                _opl.adlib_write(_opl.index, val, 0);
+            }
+
+            /*void WoodyEmuOPL::init()
+            {
+            }*/
+
+            int32_t WoodyEmuOPL::getSampleRate() const noexcept
+            {
+                return _opl.getSampleRate();
+            }
+        }
+    }
+}
---
+++ sdl2-sonic-drivers/src/hardware/opl/woody/WoodyEmuOPL.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <hardware/opl/OPL.hpp>
+#include "OPLChip.hpp"
+
+namespace hardware
+{
+    namespace opl
+    {
+        namespace woody
+        {
+            class WoodyEmuOPL : public OPL
+            {
+            public:
+                WoodyEmuOPL(const int rate, const bool usestereo) noexcept;
+              
+                virtual void update(int16_t* buf, const int32_t samples);
+                virtual void write(const int reg, const int val);
+                //virtual void init();
+                virtual int32_t getSampleRate() const noexcept;
+            private:
+                bool    _stereo;
+                OPLChip _opl;
+            };
+        }
+    }
+}
