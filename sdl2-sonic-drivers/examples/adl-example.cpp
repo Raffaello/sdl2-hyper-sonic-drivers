b25a2dfc53b77635733871893a37764c1d19713d
Raffaello Bertini <raffaellobertini@gmail.com>
2021-08-30 14:28:38 +0100

    [WIP] init

---
+++ sdl2-sonic-drivers/examples/adl-example.cpp
@@ -0,0 +1,29 @@
+#include <hardware/opl/OPL.hpp>
+#include <hardware/opl/scummvm/Config.hpp>
+#include <utils/algorithms.hpp>
+#include <audio/scummvm/SDLMixerManager.hpp>
+#include <files/ADLFile.hpp>
+
+#include <spdlog/spdlog.h>
+#include <fmt/color.h>
+
+#include <memory>
+#include <cstdint>
+#include <map>
+#include <string>
+
+
+using audio::scummvm::SdlMixerManager;
+using hardware::opl::scummvm::Config;
+using hardware::opl::scummvm::OplEmulator;
+using utils::delayMillis;
+using files::ADLFile;
+
+int main(int argc, char* argv[])
+{
+
+
+    return 0;
+}
+
+
