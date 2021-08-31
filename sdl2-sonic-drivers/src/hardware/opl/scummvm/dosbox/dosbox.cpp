8cece73ae697d31693fd851d059237dea31daf69
Raffaello Bertini <raffaellobertini@gmail.com>
2021-08-31 14:49:37 +0100

    [wip]

--- sdl2-sonic-drivers/src/hardware/opl/scummvm/dosbox/dosbox.cpp
+++ sdl2-sonic-drivers/src/hardware/opl/scummvm/dosbox/dosbox.cpp
@@ -133,7 +133,7 @@ namespace hardware
                 }
 
                 OPL::OPL(const std::shared_ptr<audio::scummvm::Mixer> mixer, Config::OplType type)
-                    : EmulatedOPL(mixer), _type(type), _rate(0), _emulator(0), _reg({ 0 })
+                    : EmulatedOPL(mixer), _type(type), _rate(0), _emulator(nullptr), _reg({ 0 })
                 {}
                
                 OPL::~OPL()
