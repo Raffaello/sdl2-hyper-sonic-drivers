35c314a89ba01f6f947a39b3f811665c12a8a42b
Raffaello Bertini <raffaellobertini@gmail.com>
2021-09-03 16:17:19 +0100

    [WIP] init

--- sdl2-sonic-drivers/src/files/MIDFile.cpp
+++ sdl2-sonic-drivers/src/files/MIDFile.cpp
@@ -1,6 +1,33 @@
-#include "ADLFile.hpp"
+#include <files/MIDFile.hpp>
 
 namespace files
 {
+    constexpr int32_t MAX_VRQ = 0xFFFFFFF; // 3.5 bytes = 27 bits
 
+    MIDFile::MIDFile(const std::string& filename) : File(filename)
+    {
+        // TODO VLQ
+
+
+
+    
+    }
+
+    MIDFile::~MIDFile()
+    {}
+
+    /*int32_t MIDFile::decode_VLQ(const int32_t encoded) const
+    {
+        union
+        {
+            int32_t v;
+            int8_t a, b, c, d;
+        } u;
+        
+        u.v = encoded;
+        int32_t result = 0;
+        
+        u.a 
+
+    }*/
 }
--- sdl2-sonic-drivers/src/files/MIDFile.hpp
+++ sdl2-sonic-drivers/src/files/MIDFile.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <files/File.hpp>
+#include <cstdint>
 
 namespace files
 {
-	class MIDFile : public File
-	{
+    class MIDFile : public File
+    {
+    public:
+        MIDFile(const std::string& filename);
+        virtual ~MIDFile();
+        /// <summary>
+        /// Variable length quantity decoding algorithm
+        /// </summary>
+        /// <param name="encoded"></param>
+        /// <returns></returns>
+        //int32_t decode_VLQ(const int32_t encoded) const;
 
-	};
+    private:
+    };
 }
