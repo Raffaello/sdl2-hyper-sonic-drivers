78ae0bec3f08199cb0887c6bd4088c590e76ffa2
Raffaello Bertini <raffaellobertini@gmail.com>
2021-09-12 11:44:54 +0100

    [WIP] WAVDriver init

---
+++ sdl2-sonic-drivers/src/drivers/WAVDriver.cpp
@@ -0,0 +1,11 @@
+#include <drivers/WAVDriver.hpp>
+
+namespace drivers
+{
+    WAVDriver::WAVDriver(std::shared_ptr<audio::scummvm::Mixer> mixer, std::shared_ptr<files::WAVFile> wav_file)
+    {
+    }
+    WAVDriver::~WAVDriver()
+    {
+    }
+}
