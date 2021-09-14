966a7a373a97e169ca65dddfa821484177f19e1f
Raffaello Bertini <raffaellobertini@gmail.com>
2021-09-14 16:46:00 +0100

    MIDParser in text version

--- sdl2-sonic-drivers/src/drivers/MIDParser.cpp
+++ sdl2-sonic-drivers/src/drivers/MIDParser.cpp
@@ -36,10 +36,6 @@ namespace drivers
         int num_tracks = _mid_file->getNumTracks();
         int division = _mid_file->getDivision();
         if (division & 0x8000) {
-            // ticks per quarter note
-            spdlog::info("Division: Ticks per quarter note = {}", division & 0x7FFF);
-        }
-        else {
             // ticks per frame
             int smpte = (division & 0x7FFF) >> 8;
             int ticksPerFrame = division & 0xFF;
@@ -50,12 +46,15 @@ namespace drivers
             case -29:
             case -30:
                 break;
-            default: 
+            default:
                 spdlog::warn("Division SMPTE not know = {}", smpte);
             }
 
             spdlog::info("Division: Ticks per frame = {}, smpte", ticksPerFrame, smpte);
-
+        }
+        else {
+            // ticks per quarter note
+            spdlog::info("Division: Ticks per quarter note = {}", division & 0x7FFF);
         }
 
         std::vector<MIDFile::MIDI_track_t> tracks = _mid_file->getTracks();
@@ -66,88 +65,121 @@ namespace drivers
             for (auto& e : track.events)
             {
                 spdlog::info("MIDI Track#={:3}, Event: dt={:4}, type={:#04x}",i, e.delta_time, e.type.val);
-                switch (e.type.val)
+                switch (e.type.high)
                 {
-                case 0xFF: // meta-event
-                {
-                    uint8_t type = e.data[0]; // must be < 128
-                    uint32_t length = e.data.size() - 1; // previously decoded, but don't know how many bytes to skip!
-                    // sanity check
-                    //uint32_t out_length;
-                    //int skip = decode_VLQ(&e.data[1], out_length) + 1; // +1 because of the [0] index = type
-                    //if (length != out_length) {
-                    //    spdlog::error("Meta-Event length error: expected {} but is {}", length, out_length);
-                    //}
-                    int skip = 1;
-                    
-                    switch (static_cast<MIDFile::MIDI_META_EVENT>(type))
+                case 0xF:
+                    switch (e.type.low)
                     {
-                    case MIDFile::MIDI_META_EVENT::CHANNEL_PREFIX:
-                        spdlog::info("Channel {}", e.data[skip]); // 0-15
-                        break;
-                    case MIDFile::MIDI_META_EVENT::COPYRIGHT:
-                        spdlog::info("Copyright {}", &e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::CUE_POINT:
-                        spdlog::info("Cue Point {}", &e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::END_OF_TRACK:
-                        spdlog::info("End Of Track");
-                        break;
-                    case MIDFile::MIDI_META_EVENT::INSTRUMENT_NAME:
-                        spdlog::info("Instrument Name {}", &e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::KEY_SIGNATURE:
-                        spdlog::info("Key: {:d},  major/minor={:d}", e.data[skip], e.data[skip + 1]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::LYRICS:
-                        spdlog::info("Lyrics {}", &e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::MARKER:
-                        spdlog::info("Marker {}", &e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::MIDI_PORT:
-                        spdlog::info("Midi Port {:d}", e.data[skip]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::SEQUENCER_SPECIFIC:
-                        spdlog::warn("Sequencer specific, skip");
-                        break;
-                    case MIDFile::MIDI_META_EVENT::SEQUENCE_NAME:
+                    case 0xF: // meta-event
                     {
-                        spdlog::info("Sequence Name '{}'", midi_event_to_string(e.data));
-                        break;
-                    }
-                    case MIDFile::MIDI_META_EVENT::SEQUENCE_NUMBER:
-                        spdlog::info("Sequence Number {:d} {:d}", e.data[skip + 1], e.data[skip + 2]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::SET_TEMPO:
-                    {
-                        uint32_t tempo = (e.data[skip] << 16) + (e.data[skip + 1] << 8) + (e.data[skip + 2]);
-                        spdlog::info("Tempo {}, ({} bps)", tempo, 60000000/tempo);
-                        break;
-                    }
-                    case MIDFile::MIDI_META_EVENT::SMPTE_OFFSET:
-                        spdlog::info("SMPTE OFFSET hh={:d}, mm={:d}, ss={:d}, frames={:d}, fractional_frame={:d}",
-                            e.data[skip], e.data[skip + 1], e.data[skip + 2], e.data[skip + 3], e.data[skip + 4]);
-                        break;
-                    case MIDFile::MIDI_META_EVENT::TEXT:
-                        spdlog::info("Text {}", midi_event_to_string(e.data));
-                        break;
-                    case MIDFile::MIDI_META_EVENT::TIME_SIGNATURE:
-                    {
-                        spdlog::info("Time Signature: {:d}/{:d}, midi_clock={}, bb={}",
-                            e.data[skip], powerOf2(e.data[skip + 1]), e.data[skip + 2], e.data[skip + 3]);
-                        break;
-                    }
-                    default: 
-                        spdlog::error("MetaEvent not know");
-                        return;
-                    }
+                        uint8_t type = e.data[0]; // must be < 128
+                        uint32_t length = e.data.size() - 1; // previously decoded, but don't know how many bytes to skip!
+                        // sanity check
+                        //uint32_t out_length;
+                        //int skip = decode_VLQ(&e.data[1], out_length) + 1; // +1 because of the [0] index = type
+                        //if (length != out_length) {
+                        //    spdlog::error("Meta-Event length error: expected {} but is {}", length, out_length);
+                        //}
+                        int skip = 1;
 
+                        switch (static_cast<MIDFile::MIDI_META_EVENT>(type))
+                        {
+                        case MIDFile::MIDI_META_EVENT::CHANNEL_PREFIX:
+                            spdlog::info("Channel {}", e.data[skip]); // 0-15
+                            break;
+                        case MIDFile::MIDI_META_EVENT::COPYRIGHT:
+                            spdlog::info("Copyright {}", &e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::CUE_POINT:
+                            spdlog::info("Cue Point {}", &e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::END_OF_TRACK:
+                            spdlog::info("End Of Track");
+                            break;
+                        case MIDFile::MIDI_META_EVENT::INSTRUMENT_NAME:
+                            spdlog::info("Instrument Name {}", &e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::KEY_SIGNATURE:
+                            spdlog::info("Key: {:d},  major/minor={:d}", e.data[skip], e.data[skip + 1]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::LYRICS:
+                            spdlog::info("Lyrics {}", &e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::MARKER:
+                            spdlog::info("Marker {}", &e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::MIDI_PORT:
+                            spdlog::info("Midi Port {:d}", e.data[skip]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::SEQUENCER_SPECIFIC:
+                            spdlog::warn("Sequencer specific, skip");
+                            break;
+                        case MIDFile::MIDI_META_EVENT::SEQUENCE_NAME:
+                        {
+                            spdlog::info("Sequence Name '{}'", midi_event_to_string(e.data));
+                            break;
+                        }
+                        case MIDFile::MIDI_META_EVENT::SEQUENCE_NUMBER:
+                            spdlog::info("Sequence Number {:d} {:d}", e.data[skip + 1], e.data[skip + 2]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::SET_TEMPO:
+                        {
+                            uint32_t tempo = (e.data[skip] << 16) + (e.data[skip + 1] << 8) + (e.data[skip + 2]);
+                            spdlog::info("Tempo {}, ({} bps)", tempo, 60000000 / tempo);
+                            break;
+                        }
+                        case MIDFile::MIDI_META_EVENT::SMPTE_OFFSET:
+                            spdlog::info("SMPTE OFFSET hh={:d}, mm={:d}, ss={:d}, frames={:d}, fractional_frame={:d}",
+                                e.data[skip], e.data[skip + 1], e.data[skip + 2], e.data[skip + 3], e.data[skip + 4]);
+                            break;
+                        case MIDFile::MIDI_META_EVENT::TEXT:
+                            spdlog::info("Text {}", midi_event_to_string(e.data));
+                            break;
+                        case MIDFile::MIDI_META_EVENT::TIME_SIGNATURE:
+                        {
+                            spdlog::info("Time Signature: {:d}/{:d}, midi_clock={}, bb={}",
+                                e.data[skip], powerOf2(e.data[skip + 1]), e.data[skip + 2], e.data[skip + 3]);
+                            break;
+                        }
+                        default:
+                            spdlog::error("MetaEvent not know");
+                            return;
+                        }
+
+                    
+                    }
                     break;
-                }
+                    case 0x0:
+                    case 0x7:
+                        spdlog::warn("SysEx Event type={:#04x}, skipped", e.type.val);
+                    }
+                break;
+
+                // 2 data values
+                case 0x8: // note off
+                    spdlog::info("Channel #{} Note OFF: note={}, velocity={}", (int)e.type.low, e.data[0], e.data[1]);
+                    break;
+                case 0x9: // note on
+                    spdlog::info("Channel #{} Note ON: note={}, velocity={}", (int)e.type.low, e.data[0], e.data[1]);
+                    break;
+                case 0xA: // note aftertouch
+                    spdlog::info("Channel #{} Note Aftertouch: note={}, ater touch value={}", (int)e.type.low, e.data[0], e.data[1]);
+                    break;
+                case 0xB: // controller
+                    spdlog::info("Channel #{} Controller: number={}, value={}", (int)e.type.low, e.data[0], e.data[1]);
+                    break;
+                case 0xE: // pitch bend
+                    spdlog::info("Channel #{} Pitch Bend: value={}", (int)e.type.low, (e.data[0]) | (e.data[1] << 8));
+                    break;
+                // 1 data values
+                case 0xC: // program change
+                    spdlog::info("Channel #{} Program change: number={}", (int)e.type.low, e.data[0]);
+                    break;
+                case 0xD: // channel aftertouch
+                    spdlog::info("Channel #{} Channel after touch: value={}", (int)e.type.low, e.data[0]);
+                    break;
                 default:
-                    spdlog::critical("event type={} parsing not implemented", e.type.val);
+                    spdlog::critical("event type={:#04x} parsing not implemented", (int)e.type.low, e.type.val);
                     return;
                 }
             }
