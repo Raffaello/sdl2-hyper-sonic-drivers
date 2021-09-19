4d78997c17199492c20b1f7f377631acec949843
Raffaello Bertini <raffaellobertini@gmail.com>
2021-09-19 13:22:59 +0100

    [WIP] need to fix the XMI fixtures

--- sdl2-sonic-drivers/src/files/miles/XMIFile.hpp
+++ sdl2-sonic-drivers/src/files/miles/XMIFile.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <files/IFFFile.hpp>
+#include <audio/midi/MIDIEvent.hpp>
 #include <string>
 #include <vector>
 #include <audio/MIDI.hpp>
@@ -11,6 +12,17 @@ namespace files
 {
     namespace miles
     {
+        /// <summary>
+        /// XMI files play MIDI at a fixed clock rate of 120 Hz.
+        /// To play them correctly, the Tempo and PPQN division values
+        /// used for playback should therefore also yield a constant 120 Hz,
+        /// for example a tempo of 500 000 microseconds and a PPQN of 60,
+        /// or a tempo of 1 000 000 microseconds and a PPQN of 120.
+        /// Tempo events contained in the EVNT chunks should be ignored;
+        /// they are vestigial remnants of the original MIDI file converted
+        /// into the XMI format and do not correspond to the values
+        /// used by the converted event stream.
+        /// </summary>
         class XMIFile final : protected IFFFile
         {
         public:
@@ -20,8 +32,8 @@ namespace files
             int getNumTracks() const noexcept;
             const std::vector<uint8_t>& getTrack(const uint16_t track) const noexcept;
         private:
-            void _readFormXdirChunk();
-            void _readEvnt(const IFF_sub_chunk_header_t& IFF_evnt, const int16_t track);
+            uint16_t _readFormXdirChunk(IFF_chunk_header_t& form_xdir);
+            audio::midi::MIDIEvent _readEvnt(const IFF_sub_chunk_header_t& IFF_evnt, const int16_t track);
             void _readTimb(const IFF_sub_chunk_header_t& IFF_timb, const int16_t track);
             void _readRbrn(const IFF_sub_chunk_header_t& IFF_rbrn, const int16_t track);
             
