0dd7544542d816277244d2383c5aa1349ba5b1d9
Raffaello Bertini <raffaellobertini@gmail.com>
2021-10-01 19:45:02 +0100

    pcm-exmaple code review

--- sdl2-sonic-drivers/examples/pcm-example.cpp
+++ sdl2-sonic-drivers/examples/pcm-example.cpp
@@ -5,73 +5,13 @@
 #include <files/VOCFile.hpp>
 #include <audio/Sound.hpp>
 
-#include <spdlog/spdlog.h>
+#include <iostream>
 #include <SDL2/SDL.h>
 
 using namespace audio::scummvm;
-using  drivers::PCMDriver;
-
-
-int voc_exmaple()
-{
-    SdlMixerManager mixerManager;
-    mixerManager.init();
-
-    std::shared_ptr<Mixer> mixer = mixerManager.getMixer();
-
-    std::shared_ptr<files::VOCFile> vocFile = std::make_shared<files::VOCFile>("DUNE.VOC");
-
-    PCMDriver voc(mixer);
-    voc.play(vocFile->getSound());
-
-    while (!mixer->isReady()) {
-        spdlog::info("mixer not ready");
-        SDL_Delay(100);
-    }
-
-    while (voc.isPlaying(vocFile->getSound()))
-    {
-        spdlog::info("is playing");
-        SDL_Delay(1000);
-
-    }
-
-    spdlog::info("quitting...");
-    SDL_Delay(1000);
-
-    return 0;
-}
-
-int wav_example()
-{
-    SdlMixerManager mixerManager;
-    mixerManager.init();
-
-    std::shared_ptr<Mixer> mixer = mixerManager.getMixer();
-
-    std::shared_ptr<files::WAVFile> wavFile = std::make_shared<files::WAVFile>("Wav_868kb.wav");
-    std::shared_ptr<audio::Sound> wavSound = wavFile->getSound();
-
-    PCMDriver wav(mixer);
-
-    wav.play(wavSound);
-
-    while (!mixer->isReady()) {
-        spdlog::info("mixer not ready");
-        SDL_Delay(100);
-    }
-
-    while (wav.isPlaying(wavSound))
-    {
-        spdlog::info("is playing");
-        SDL_Delay(1000);
-    }
-
-    spdlog::info("quitting...");
-    SDL_Delay(1000);
-
-    return 0;
-}
+using drivers::PCMDriver;
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[])
 {
@@ -79,25 +19,22 @@ int main(int argc, char* argv[])
     mixerManager.init();
 
     std::shared_ptr<Mixer> mixer = mixerManager.getMixer();
-
     std::shared_ptr<files::WAVFile> wavFile = std::make_shared<files::WAVFile>("Wav_868kb.wav");
     std::shared_ptr<files::VOCFile> vocFile = std::make_shared<files::VOCFile>("DUNE.VOC");
-
     std::shared_ptr<audio::Sound> wavSound = wavFile->getSound();
     std::shared_ptr<audio::Sound> vocSound = vocFile->getSound();
 
     PCMDriver drv(mixer);
 
-    drv.play(wavSound);
-
     while (!mixer->isReady()) {
-        spdlog::info("mixer not ready");
+        cout << "mixer not ready" << endl;
         SDL_Delay(100);
     }
 
+    drv.play(wavSound);
     while(drv.isPlaying(wavSound))
     {
-        spdlog::info("is playing");
+        cout << "is playing" << endl;
         SDL_Delay(1000);
     }
 
@@ -106,9 +43,8 @@ int main(int argc, char* argv[])
     drv.play(vocSound);
     while (drv.isPlaying(vocSound))
     {
-        spdlog::info("is playing");
+        cout << "is playing" << endl;
         SDL_Delay(1000);
-
     }
 
     SDL_Delay(500);
@@ -117,16 +53,9 @@ int main(int argc, char* argv[])
     drv.play(vocSound);
     while(drv.isPlaying(vocSound) || drv.isPlaying(wavSound))
     {
-        spdlog::info("is playing");
+        cout << "is playing" << endl;
         SDL_Delay(1000);
     }
 
     return 0;
-
-
-    return 0;
-
-
-
-
 }
