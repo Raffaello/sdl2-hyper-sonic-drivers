5d7b146aca2097cc1ef6668b4ea9cfcabfa6e67a
Raffaello Bertini <raffaellobertini@gmail.com>
2022-08-31 22:05:23 +0100

    remove MIDIEvent.cpp

--- sdl2-sonic-drivers/src/audio/midi/MIDITrack.hpp
+++ sdl2-sonic-drivers/src/audio/midi/MIDITrack.hpp
@@ -11,6 +11,7 @@ namespace audio
         {
         public:
             MIDITrack() = default;
+            ~MIDITrack() = default;
             MIDITrack(const std::vector<MIDIEvent> events);
             void addEvent(const MIDIEvent& e);
 
