14b867d16157e13468f61d1d871f05f8331bd145
Raffaello Bertini <raffaellobertini@gmail.com>
2022-09-21 22:46:28 +0100

    code rev

--- sdl2-sonic-drivers/src/hardware/opl/scummvm/mame/mame.cpp
+++ sdl2-sonic-drivers/src/hardware/opl/scummvm/mame/mame.cpp
@@ -47,59 +47,6 @@ using utils::CLIP;
 
 namespace hardware::opl::scummvm::mame
 {
-    /*OPL::OPL(const OplType type, const std::shared_ptr<audio::scummvm::Mixer>& mixer)
-        : EmulatedOPL(type, mixer)
-    {
-        if (type != OplType::OPL2) {
-            throw std::runtime_error("ScummVM::MAME only support OPL2");
-        }
-    }
-
-    OPL::~OPL() {
-        stop();
-        OPLDestroy(_opl);
-        _opl = nullptr;
-    }
-
-    bool OPL::init()
-    {
-        if (_init)
-        {
-            stopCallbacks();
-            OPLDestroy(_opl);
-        }
-
-        _opl = makeAdLibOPL(_mixer->getOutputRate());
-
-        _init = (_opl != nullptr);
-        return _init;
-    }
-
-    void OPL::reset() {
-        OPLResetChip(_opl);
-    }
-
-    void OPL::write(int a, int v) {
-        OPLWrite(_opl, a, v);
-    }
-
-    uint8_t OPL::read(int a) {
-        return OPLRead(_opl, a);
-    }
-
-    void OPL::writeReg(int r, int v) {
-        OPLWriteReg(_opl, r, v);
-    }
-
-    bool OPL::isStereo() const
-    {
-        return false;
-    }
-
-    void OPL::generateSamples(int16_t* buffer, int length) {
-        YM3812UpdateOne(_opl, buffer, length);
-    }*/
-
     /* -------------------- preliminary define section --------------------- */
     /* attack/decay rate time rate */
 #define OPL_ARRATE     141280  /* RATE 4 =  2826.24ms @ 3.6MHz */
--- sdl2-sonic-drivers/src/hardware/opl/scummvm/mame/mame.hpp
+++ sdl2-sonic-drivers/src/hardware/opl/scummvm/mame/mame.hpp
@@ -147,27 +147,4 @@ namespace hardware::opl::scummvm::mame
 
     // Factory method
     FM_OPL* makeAdLibOPL(int rate);
-
-    //// OPL API implementation
-    //class OPL : public EmulatedOPL
-    //{
-    //private:
-    //    FM_OPL* _opl = nullptr;
-    //public:
-    //    explicit OPL(const OplType type, const std::shared_ptr<audio::scummvm::Mixer>& mixer);
-    //    virtual ~OPL();
-
-    //    bool init() override;
-    //    void reset() override;
-
-    //    void write(int a, int v) override;
-    //    uint8_t read(int a) override;
-
-    //    void writeReg(int r, int v) override;
-
-    //    bool isStereo() const override;
-
-    //protected:
-    //    void generateSamples(int16_t* buffer, int length) override;
-    //};
 }
