2fe82aa505e3ee226cd508e85e9ca60adfdc3a03
Raffaello Bertini <raffaellobertini@gmail.com>
2023-09-06 23:05:22 +0100

    [WIP] new mixer

---
+++ sdl2-hyper-sonic-drivers/src/HyperSonicDrivers/audio/sdl2/MixerManager.cpp
@@ -0,0 +1,19 @@
+#include <HyperSonicDrivers/audio/sdl2/MixerManager.hpp>
+#include <sdl2/SDL_audio.h>
+
+namespace HyperSonicDrivers::audio::sdl2
+{
+    bool MixerManager::init()
+    {
+        return false;
+    }
+
+    void MixerManager::suspendMixer()
+    {
+       
+    }
+
+    void MixerManager::resumeMixer()
+    {
+    }
+}
