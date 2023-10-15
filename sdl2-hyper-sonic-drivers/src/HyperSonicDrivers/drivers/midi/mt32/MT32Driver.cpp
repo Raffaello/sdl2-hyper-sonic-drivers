d9196aedc4d343ff6ef983641921d392b630832c
Raffaello Bertini <raffaellobertini@gmail.com>
2023-10-15 16:46:51 +0100

    fix build

--- sdl2-hyper-sonic-drivers/src/HyperSonicDrivers/drivers/midi/mt32/MT32Driver.cpp
+++ sdl2-hyper-sonic-drivers/src/HyperSonicDrivers/drivers/midi/mt32/MT32Driver.cpp
@@ -53,14 +53,4 @@ namespace HyperSonicDrivers::drivers::midi::mt32
     void MT32Driver::onCallback() noexcept
     {
     }
-
-    void MT32Driver::noteOff(const uint8_t chan, const uint8_t note) noexcept
-    {
-        utils::throwLogC<std::runtime_error>("not implemented");
-    }
-
-    void MT32Driver::noteOn(const uint8_t chan, const uint8_t note, const uint8_t vol) noexcept
-    {
-        utils::throwLogC<std::runtime_error>("not implemented");
-    }
 }
