43fd9e0865ac674bb61250f587ebb04935c3c187
Raffaello Bertini <raffaellobertini@gmail.com>
2023-10-19 12:26:33 +0100

    eChannelGroupFormatter

---
+++ sdl2-hyper-sonic-drivers/src/std/eChannelGroupFormatter.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <format>
+#include <HyperSonicDrivers/audio/mixer/ChannelGroup.hpp>
+
+namespace std
+{
+    template<>
+    struct formatter<HyperSonicDrivers::audio::mixer::eChannelGroup> : formatter<string_view>
+    {
+        template<typename FormatContext>
+        auto format(HyperSonicDrivers::audio::mixer::eChannelGroup ch_group, FormatContext& fc) const
+        {
+            string str;
+
+            switch (emu)
+            {
+                using enum HyperSonicDrivers::audio::mixers::eChannelGroup;
+
+            case Plain:
+                str = "Plain";
+                break;
+            case Music:
+                str = "Music";
+                break;
+            case Sfx:
+                str = "Sfx";
+                break;
+            case Speech:
+                str = "Speech";
+            case Unknown:
+                str = "Unknown";
+                break;
+            }
+
+            return formatter<std::string_view>::format(str, fc);
+        }
+    };
+}
