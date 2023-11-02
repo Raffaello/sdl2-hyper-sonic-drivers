72c793af95137a639d24116d2bf2f06ce2ddc22a
Raffaello Bertini <raffaellobertini@gmail.com>
2023-11-02 11:00:31 +0000

    code rev

--- sdl2-hyper-sonic-drivers/src/HyperSonicDrivers/audio/sdl2/Renderer.hpp
+++ sdl2-hyper-sonic-drivers/src/HyperSonicDrivers/audio/sdl2/Renderer.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
-#include <HyperSonicDrivers/audio/IRenderer.hpp>
-#include <HyperSonicDrivers/audio/IMixer.hpp>
-#include <HyperSonicDrivers/audio/IAudioStream.hpp>
-#include <HyperSonicDrivers/files/WAVFile.hpp>
 #include <cstdint>
-#include <vector>
-#include <memory>
 #include <filesystem>
+#include <HyperSonicDrivers/audio/IRenderer.hpp>
+#include <HyperSonicDrivers/audio/IAudioStream.hpp>
 
 namespace HyperSonicDrivers::audio::sdl2
 {
@@ -22,8 +18,5 @@ namespace HyperSonicDrivers::audio::sdl2
 
         void renderBuffer(IAudioStream* stream) override;
         using IRenderer::renderBuffer;
-    private:
-        std::unique_ptr<files::WAVFile> m_out;
-        std::vector<int16_t> m_buf;
     };
 }
